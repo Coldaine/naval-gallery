d85056657d5fd27f270174b0cf62c76b0eac1d66
Coldaine <coldaine@example.com>
2026-01-01 13:24:22 -0600

    feat(data): add image manifests and catalog

---
+++ data/images.js
@@ -0,0 +1,188 @@
+const images = [
+  {
+    "id": "loc_unknown",
+    "title": "Engineering drawings for a submarine (\"Submarine Vessel, Submarine Bombs and Mode of Attack\") for the United States government",
+    "url": "https://www.loc.gov/static/images/original-format/group-of-images.svg",
+    "local_path": "img/loc/loc_unknown.jpg",
+    "source": "Library of Congress",
+    "type": "profile"
+  },
+  {
+    "id": "loc_unknown",
+    "title": "The pie rat ship",
+    "url": "https://tile.loc.gov/storage-services/service/pnp/ade/2a02000/2a02900/2a02919r.jpg#h=640&w=567",
+    "local_path": "img/loc/loc_unknown.jpg",
+    "source": "Library of Congress",
+    "type": "profile"
+  },
+  {
+    "id": "loc_unknown",
+    "title": "[Cass Gilbert sleeping on ship deck, S.S. Friesland]",
+    "url": "https://tile.loc.gov/storage-services/service/pnp/ade/2a03000/2a03500/2a03521r.jpg#h=640&w=462",
+    "local_path": "img/loc/loc_unknown.jpg",
+    "source": "Library of Congress",
+    "type": "profile"
+  },
+  {
+    "id": "loc_unknown",
+    "title": "[Submarine (\"Submarine Vessel, Submarine Bombs and Mode of Attack\") for the United States government.] [View of ships and mines]",
+    "url": "https://tile.loc.gov/storage-services/service/pnp/ppmsca/83900/83910v.jpg#h=726&w=1024",
+    "local_path": "img/loc/loc_unknown.jpg",
+    "source": "Library of Congress",
+    "type": "profile"
+  },
+  {
+    "id": "loc_unknown",
+    "title": "[Submarine (\"Submarine Vessel, Submarine Bombs and Mode of Attack\") for the United States government.] [Manner of arranging ropes]",
+    "url": "https://tile.loc.gov/storage-services/service/pnp/ppmsca/83900/83915v.jpg#h=768&w=1024",
+    "local_path": "img/loc/loc_unknown.jpg",
+    "source": "Library of Congress",
+    "type": "profile"
+  },
+  {
+    "id": "loc_unknown",
+    "title": "Welder making boilers for a ship, Combustion Engineering Co., Chattanooga, Tenn.",
+    "url": "https://tile.loc.gov/storage-services/service/pnp/fsac/1a35000/1a35200/1a35229v.jpg#h=828&w=1024",
+    "local_path": "img/loc/loc_unknown.jpg",
+    "source": "Library of Congress",
+    "type": "profile"
+  },
+  {
+    "id": "loc_unknown",
+    "title": "Welder making boilers for a ship, Combustion Engineering Co., Chattanooga, Tenn.",
+    "url": "https://tile.loc.gov/storage-services/service/pnp/fsac/1a35000/1a35200/1a35230v.jpg#h=837&w=1024",
+    "local_path": "img/loc/loc_unknown.jpg",
+    "source": "Library of Congress",
+    "type": "profile"
+  },
+  {
+    "id": "loc_unknown",
+    "title": "Welder making boilers for a ship, Combustion Engineering Co., Chattanooga, Tenn.",
+    "url": "https://tile.loc.gov/storage-services/service/pnp/fsac/1a35000/1a35200/1a35231v.jpg#h=834&w=1024",
+    "local_path": "img/loc/loc_unknown.jpg",
+    "source": "Library of Congress",
+    "type": "profile"
+  },
+  {
+    "id": "loc_unknown",
+    "title": "Welder making boilers for a ship, Combustion Engineering Co., Chattanooga, Tenn.",
+    "url": "https://tile.loc.gov/storage-services/service/pnp/fsac/1a35000/1a35200/1a35232v.jpg#h=828&w=1024",
+    "local_path": "img/loc/loc_unknown.jpg",
+    "source": "Library of Congress",
+    "type": "profile"
+  },
+  {
+    "id": "loc_unknown",
+    "title": "Welders making boilers for a ship, Combustion Engineering Co., Chattanooga, Tenn.",
+    "url": "https://tile.loc.gov/storage-services/service/pnp/fsac/1a35000/1a35200/1a35233v.jpg#h=829&w=1024",
+    "local_path": "img/loc/loc_unknown.jpg",
+    "source": "Library of Congress",
+    "type": "profile"
+  },
+  {
+    "id": "wiki_47040388",
+    "title": "Balticprincess 2008gaplan.jpg",
+    "url": "https://upload.wikimedia.org/wikipedia/commons/5/52/Balticprincess_2008gaplan.jpg",
+    "desc": "galaxy cruise class eckplans taken from hhvferry",
+    "date": "2016-02-18",
+    "source": "Wikimedia Commons",
+    "local_path": "img/wiki/wiki_47040388.jpg"
+  },
+  {
+    "id": "wiki_80554798",
+    "title": "North Carolina and Delaware - Profile Showing Method of Strengthening - NARA - 68865606.jpg",
+    "url": "https://upload.wikimedia.org/wikipedia/commons/a/af/North_Carolina_and_Delaware_-_Profile_Showing_Method_of_Strengthening_-_NARA_-_68865606.jpg",
+    "desc": "Unknown",
+    "date": "1827<div style=\"display: none;\">date QS:P571,+1827-00-00T00:00:00Z/9</div>",
+    "source": "Wikimedia Commons",
+    "local_path": "img/wiki/wiki_80554798.jpg"
+  },
+  {
+    "id": "ERIC_ED250052_10",
+    "item_id": "ERIC_ED250052",
+    "leaf": "10",
+    "url": "https://archive.org/download/ERIC_ED250052/page/n10.jpg",
+    "title": "ONI Plate (Leaf 10)",
+    "source": "ERIC_ED250052",
+    "navy": "Unknown",
+    "type": "profile",
+    "local_path": "img/oni/ERIC_ED250052_10.jpg"
+  },
+  {
+    "id": "ERIC_ED250052_15",
+    "item_id": "ERIC_ED250052",
+    "leaf": "15",
+    "url": "https://archive.org/download/ERIC_ED250052/page/n15.jpg",
+    "title": "ONI Plate (Leaf 15)",
+    "source": "ERIC_ED250052",
+    "navy": "Unknown",
+    "type": "profile",
+    "local_path": "img/oni/ERIC_ED250052_15.jpg"
+  },
+  {
+    "id": "ERIC_ED250052_20",
+    "item_id": "ERIC_ED250052",
+    "leaf": "20",
+    "url": "https://archive.org/download/ERIC_ED250052/page/n20.jpg",
+    "title": "ONI Plate (Leaf 20)",
+    "source": "ERIC_ED250052",
+    "navy": "Unknown",
+    "type": "profile",
+    "local_path": "img/oni/ERIC_ED250052_20.jpg"
+  },
+  {
+    "id": "FM30-301956_10",
+    "item_id": "FM30-301956",
+    "leaf": "10",
+    "url": "https://archive.org/download/FM30-301956/page/n10.jpg",
+    "title": "ONI Plate (Leaf 10)",
+    "source": "FM30-301956",
+    "navy": "Unknown",
+    "type": "profile",
+    "local_path": "img/oni/FM30-301956_10.jpg"
+  },
+  {
+    "id": "FM30-301956_15",
+    "item_id": "FM30-301956",
+    "leaf": "15",
+    "url": "https://archive.org/download/FM30-301956/page/n15.jpg",
+    "title": "ONI Plate (Leaf 15)",
+    "source": "FM30-301956",
+    "navy": "Unknown",
+    "type": "profile",
+    "local_path": "img/oni/FM30-301956_15.jpg"
+  },
+  {
+    "id": "FM30-301956_20",
+    "item_id": "FM30-301956",
+    "leaf": "20",
+    "url": "https://archive.org/download/FM30-301956/page/n20.jpg",
+    "title": "ONI Plate (Leaf 20)",
+    "source": "FM30-301956",
+    "navy": "Unknown",
+    "type": "profile",
+    "local_path": "img/oni/FM30-301956_20.jpg"
+  },
+  {
+    "id": "FM30-301956_25",
+    "item_id": "FM30-301956",
+    "leaf": "25",
+    "url": "https://archive.org/download/FM30-301956/page/n25.jpg",
+    "title": "ONI Plate (Leaf 25)",
+    "source": "FM30-301956",
+    "navy": "Unknown",
+    "type": "profile",
+    "local_path": "img/oni/FM30-301956_25.jpg"
+  },
+  {
+    "id": "FM30-301956_30",
+    "item_id": "FM30-301956",
+    "leaf": "30",
+    "url": "https://archive.org/download/FM30-301956/page/n30.jpg",
+    "title": "ONI Plate (Leaf 30)",
+    "source": "FM30-301956",
+    "navy": "Unknown",
+    "type": "profile",
+    "local_path": "img/oni/FM30-301956_30.jpg"
+  }
+];
